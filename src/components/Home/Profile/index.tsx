88059f86daf3d807eaa02bf6fa1eaeb78fb0742d
IAmTheMage <gustavogoncalvessilva2003@outlook.com>
2021-04-28 01:15:07 -0300

    Editing Profile page

--- src/components/Home/Profile/index.tsx
+++ src/components/Home/Profile/index.tsx
@@ -1,12 +1,17 @@
 import React, {useState} from 'react';
 import {Modal} from 'react-native';
-import { Container } from './styles';
+import { Container, Title, Info } from './styles';
 
 const Profile: React.FC = () => {
 
   return (
     <Container>
-      
+      <Title>Perfil</Title>
+      <Info>Nome: Gustavo Gonçalves Silva</Info>
+      <Info>Exercicios feitos por hora: 2 horas</Info>
+      <Info>Número de exercicios feitos: 25</Info>
+      <Info>Series completas: 8</Info>
+      <Info>Conta criada em: 26/04/2021</Info>
     </Container>
   );
 }
--- src/components/Home/Profile/styles.tsx
+++ src/components/Home/Profile/styles.tsx
@@ -6,9 +6,25 @@ const height = Dimensions.get('window').height;
 
 
 export const Container = styled.View`
-  width: ${width}px;
+  width: 100%;
   display: flex;
   height: 100%;
   background-color: #0f0f0f;
-  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-family: 'PoppinsBold';
+  margin-left: 10px;
+  color: white;
+  margin-top: 28px;
+`;
+
+export const Info = styled.Text`
+  font-size: 18px;
+  font-family: 'PoppinsRegular';
+  margin-left: 10px;
+  color: white;
+  margin-top: 6px;
+  margin-top: 20px;
 `;
