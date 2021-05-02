2104aefdb018917f3c0c3607fa1988af91f27050
IAmTheMage <gustavogoncalvessilva2003@outlook.com>
2021-05-02 14:58:13 -0300

    Add license and update Profile Page

--- src/components/Home/Profile/index.tsx
+++ src/components/Home/Profile/index.tsx
@@ -1,10 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { Alert, Modal, Dimensions } from 'react-native';
 import { Container, Title, Info, IconContainer, 
-  CenterHorizontaly, ProfileImage, CameraContainer, Row, CameraIcon } from './styles';
-import firebase, {Database} from '../../../env/firebase';
+  CenterHorizontaly, ProfileImage, CameraContainer, CameraIcon, Center, ProfileName, InformationsContainerRow, InformationContainerColumn, Information } from './styles';
+import firebase, {Database, Storage} from '../../../env/firebase';
 import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
@@ -18,14 +20,13 @@ const CameraComponent: React.FC = () => {
 }
 
 const Profile: React.FC = () => {
+  const [cameraRef, setCameraRef] = useState<any>();
   const [displayName, setDisplayName] = useState<string>("");
   const [exercises, setExercises] = useState<string>("");
   const [date, setDate] = useState<string>("");
-  const [displayImage, setDisplayImage] = useState<string>("");
+  const [displayImage, setDisplayImage] = useState<any>();
   const [contentIsLoad, setContentIsLoad] = useState<boolean>(false);
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  const [isCameraOpen, setIsCameraOpen] = useState<boolean>(false);
+  const [series, setSeries] = useState<number>(0);
 
 
   useEffect(() => {
@@ -34,12 +35,13 @@ const Profile: React.FC = () => {
   }, [])
 
   async function getUserInfo() {
+    setDisplayImage(firebase.auth().currentUser?.photoURL || "");
     const userInfo = await Database.collection('users').doc(firebase.auth().currentUser?.uid).get();
     const userData = userInfo.data();
     setExercises(userData?.exercisesCompleted);
+    setSeries(userData?.seriesCompleted)
     const realDate: string = convertDate(userData?.date)
     setDate(realDate);
-    setDisplayImage(firebase.auth().currentUser?.photoURL || "");
     setContentIsLoad(true);
   }
 
@@ -59,37 +61,50 @@ const Profile: React.FC = () => {
     return false;
   }
   
-  function pickImage() {
-    Alert.alert(
-      'Selecione uma imagem',
-      'Por favor selecione uma imagem',
-      [
-        {
-          text: 'Câmera',
-          onPress: async () => {
-            const {status} = await Camera.requestPermissionsAsync();
-            if(status === 'granted') {
-              setIsCameraOpen(true);
-            }
-          }
-        }
-      ]
-    );
+  async function pickImage() {
+    const {status} = await ImagePicker.requestCameraPermissionsAsync();
+    if(status === 'granted') {
+      const image = await ImagePicker.launchCameraAsync();
+      if(image.cancelled) {
+        return false;
+      }
+      setDisplayImage(image.uri);
+      uploadImage(image.uri);
+    }
+  }
+
+  async function uploadImage(uri: any) {
+    const resp = await fetch(uri);
+    const blob = await resp.blob();
+    const ref = Storage.ref().child(`/profileImages/${firebase.auth().currentUser?.uid}`);
+    const pushed = await ref.put(blob);
+    const url = await pushed.ref.getDownloadURL();
+    await firebase.auth().currentUser?.updateProfile({
+      photoURL: url
+    })
+  }
+
+  async function takePicture() {
+    console.log("Camera tirou foto");
+    cameraRef.takePictureAsync({onPictureSave: pictureSave});
+  }
+
+  function pictureSave(photo: any) {
+    console.log(photo);
+  }
+
+  function renderImage() {
+    console.log(displayImage);
+    return (
+      <Center>
+        <ProfileImage source={{uri: displayImage}}></ProfileImage>
+      </Center>
+    )
   }
 
   return (
     <>
-      {
-        isCameraOpen ?
-        <>
-          <Camera style={{flex: 1}}></Camera>
-          <Row>
-            <MaterialCommunityIcons style={{marginTop: height - 100, marginLeft: 10}} size={40} color={"white"} name="close"></MaterialCommunityIcons>
-            <MaterialCommunityIcons style={{marginTop: height - 100}} size={40} color={"white"} name="circle"></MaterialCommunityIcons>
-          </Row>
-        </>
-        :
-        <Container>
+      <Container>
           <CenterHorizontaly>
             {!renderDisplayImage() ?
               <IconContainer onPress={() => {
@@ -98,11 +113,23 @@ const Profile: React.FC = () => {
                 <MaterialIcons name="person" size={34} color="white" />
               </IconContainer>
               :
-              <></>
+              <>
+                {renderImage()}
+                <ProfileName>{firebase.auth().currentUser?.displayName}</ProfileName>
+                <InformationsContainerRow>
+                  <InformationContainerColumn>
+                    <Information>Exercicios completos: </Information>
+                    <Information>{exercises}</Information>
+                  </InformationContainerColumn>
+                  <InformationContainerColumn>
+                    <Information>Series completas: </Information>
+                    <Information>{series}</Information>
+                  </InformationContainerColumn>
+                </InformationsContainerRow>
+              </>
             }
           </CenterHorizontaly>
         </Container>
-      }
     </>
   );
 }
