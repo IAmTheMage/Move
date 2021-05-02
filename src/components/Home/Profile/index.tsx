ca00115e10b0f2b352e8eb74a01670bf06fa41e7
IAmTheMage <gustavogoncalvessilva2003@outlook.com>
2021-05-02 03:33:06 -0300

    Add camera and modify profile page

--- src/components/Home/Profile/index.tsx
+++ src/components/Home/Profile/index.tsx
@@ -1,11 +1,32 @@
 import React, {useState, useEffect} from 'react';
-import { Container, Title, Info } from './styles';
+import { Alert, Modal, Dimensions } from 'react-native';
+import { Container, Title, Info, IconContainer, 
+  CenterHorizontaly, ProfileImage, CameraContainer, Row, CameraIcon } from './styles';
 import firebase, {Database} from '../../../env/firebase';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Camera } from 'expo-camera';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+const CameraComponent: React.FC = () => {
+  return (
+    <CameraContainer>
+      <Camera style={{flex: 1}}></Camera>
+    </CameraContainer>
+  )
+}
 
 const Profile: React.FC = () => {
   const [displayName, setDisplayName] = useState<string>("");
   const [exercises, setExercises] = useState<string>("");
   const [date, setDate] = useState<string>("");
+  const [displayImage, setDisplayImage] = useState<string>("");
+  const [contentIsLoad, setContentIsLoad] = useState<boolean>(false);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [isCameraOpen, setIsCameraOpen] = useState<boolean>(false);
+
 
   useEffect(() => {
     setDisplayName(firebase.auth().currentUser?.displayName || "");
@@ -18,6 +39,8 @@ const Profile: React.FC = () => {
     setExercises(userData?.exercisesCompleted);
     const realDate: string = convertDate(userData?.date)
     setDate(realDate);
+    setDisplayImage(firebase.auth().currentUser?.photoURL || "");
+    setContentIsLoad(true);
   }
 
   function convertDate(date: any) {
@@ -29,14 +52,58 @@ const Profile: React.FC = () => {
     return date;
   }
 
+  function renderDisplayImage() {
+    if(contentIsLoad && displayImage) {
+      return true;
+    }
+    return false;
+  }
+  
+  function pickImage() {
+    Alert.alert(
+      'Selecione uma imagem',
+      'Por favor selecione uma imagem',
+      [
+        {
+          text: 'Câmera',
+          onPress: async () => {
+            const {status} = await Camera.requestPermissionsAsync();
+            if(status === 'granted') {
+              setIsCameraOpen(true);
+            }
+          }
+        }
+      ]
+    );
+  }
+
   return (
-    <Container>
-      <Title>Perfil</Title>
-      <Info>Nome: {displayName}</Info>
-      <Info>Número de exercicios feitos: {exercises}</Info>
-      <Info>Series completas: 8</Info>
-      <Info>Conta criada em: {date}</Info>
-    </Container>
+    <>
+      {
+        isCameraOpen ?
+        <>
+          <Camera style={{flex: 1}}></Camera>
+          <Row>
+            <MaterialCommunityIcons style={{marginTop: height - 100, marginLeft: 10}} size={40} color={"white"} name="close"></MaterialCommunityIcons>
+            <MaterialCommunityIcons style={{marginTop: height - 100}} size={40} color={"white"} name="circle"></MaterialCommunityIcons>
+          </Row>
+        </>
+        :
+        <Container>
+          <CenterHorizontaly>
+            {!renderDisplayImage() ?
+              <IconContainer onPress={() => {
+                pickImage();
+              }}>
+                <MaterialIcons name="person" size={34} color="white" />
+              </IconContainer>
+              :
+              <></>
+            }
+          </CenterHorizontaly>
+        </Container>
+      }
+    </>
   );
 }
 
